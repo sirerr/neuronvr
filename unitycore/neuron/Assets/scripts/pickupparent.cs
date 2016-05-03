c3b326d06e203935c1233ffb5b5dc19b5d721b97
bobhenz <VR@lexmark.com>
2016-05-03 19:39:35 -0400

    getting started using the space, added sword

---
+++ unitycore/neuron/Assets/scripts/pickupparent.cs
@@ -0,0 +1,96 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(SteamVR_TrackedObject))]
+public class pickupparent : MonoBehaviour {
+
+	SteamVR_TrackedObject trackedobj;
+	SteamVR_Controller.Device device;
+
+	public Transform sphere;
+
+	void Awake () {
+		trackedobj = GetComponent<SteamVR_TrackedObject>();
+	}
+
+
+	void FixedUpdate () {
+		device = SteamVR_Controller.Input((int)trackedobj.index);
+		if(device.GetTouch(SteamVR_Controller.ButtonMask.Trigger))
+		{
+			Debug.Log("you are holding 'Touch' the trigger");
+		}
+
+		if(device.GetTouchDown(SteamVR_Controller.ButtonMask.Trigger))
+		{
+			Debug.Log("you have activated 'touchdown' the trigger");
+		}
+
+		if(device.GetTouchUp(SteamVR_Controller.ButtonMask.Trigger))
+		{
+			Debug.Log("you have activated 'touchdown' the trigger");
+		}
+
+		if(device.GetPress(SteamVR_Controller.ButtonMask.Trigger))
+		{
+			Debug.Log("you are holding 'Touch' the trigger");
+		}
+
+		if(device.GetPressDown(SteamVR_Controller.ButtonMask.Trigger))
+		{
+			Debug.Log("you have activated 'touchdown' the trigger");
+		}
+
+		if(device.GetPressUp(SteamVR_Controller.ButtonMask.Trigger))
+		{
+			Debug.Log("you have activated 'touchdown' the trigger");
+		}
+
+		if(device.GetPressUp(SteamVR_Controller.ButtonMask.Touchpad))
+		{
+			Debug.Log("you have activated 'pressdown' the touchpad");
+			sphere.transform.position = new Vector3(0,0,0);
+			sphere.GetComponent<Rigidbody>().velocity  = Vector3.zero;
+			sphere.GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
+			sphere = null;
+		}
+	}
+
+	void OnTriggerStay(Collider col)
+	{
+		Debug.Log("you have collided with "+ col.name + " and activated Ontriggerstay");
+
+		if(device.GetTouch(SteamVR_Controller.ButtonMask.Trigger))
+		{
+			sphere = col.gameObject.transform;
+			Debug.Log("you have collided with col.name");
+			col.attachedRigidbody.isKinematic = true;
+			col.gameObject.transform.SetParent(gameObject.transform);
+		}
+		if(device.GetTouchUp(SteamVR_Controller.ButtonMask.Trigger))
+		{
+			
+			Debug.Log("you have released touch while colliding with the collider");
+			col.gameObject.transform.SetParent(null);
+			col.attachedRigidbody.isKinematic = false;
+
+			tossobject(col.attachedRigidbody);
+		}
+
+
+	}
+	void tossobject(Rigidbody rigidbody)
+	{
+		Transform origin = trackedobj.origin ? trackedobj.origin : trackedobj.transform.parent;
+		if(origin!=null)
+		{
+			rigidbody.velocity = origin.TransformVector(device.velocity);
+			rigidbody.angularVelocity = origin.TransformVector(device.angularVelocity);
+		}else
+		{
+			rigidbody.velocity = device.velocity;
+			rigidbody.angularVelocity = device.angularVelocity;	
+		}
+
+	}
+}
---
+++ unitycore/neuron/Assets/scripts/testareamanager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class testareamanager : MonoBehaviour {
+
+	public GameObject camerarig;
+	public SteamVR_PlayArea playarearef;
+
+	void Awake()
+	{
+		camerarig = GameObject.FindGameObjectWithTag("MainCamera");
+		playarearef = camerarig.GetComponent<SteamVR_PlayArea> ();
+
+	}
+
+	// Use this for initialization
+	void Start () {
+		playarearef = camerarig.GetComponent<SteamVR_PlayArea> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
