ebadcaa89483d02e2fb7c9305c33adb549907f63
bobhenz <VR@lexmark.com>
2016-05-18 17:05:39 -0400

    up to date for the repo

---
+++ unitycore/neuron/Assets/scripts/handanimcontrol.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(SteamVR_TrackedObject))]
+
+public class handanimcontrol : MonoBehaviour {
+
+	SteamVR_TrackedObject trackedobj;
+	SteamVR_Controller.Device device;
+	//hand object
+	public GameObject handobj;
+	//animator controller
+	private Animator handanimator;
+	// Use this for initialization
+	void Awake () {
+		trackedobj = GetComponent<SteamVR_TrackedObject> ();
+		handobj = transform.GetChild (1).gameObject;
+		handanimator = handobj.GetComponent<Animator> ();
+
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+	
+		device = SteamVR_Controller.Input((int)trackedobj.index);
+
+		if (device.GetPressDown (SteamVR_Controller.ButtonMask.Grip)) 
+		{
+			handanimator.SetTrigger("hold");
+		}
+		if (device.GetPressUp (SteamVR_Controller.ButtonMask.Grip)) 
+		{
+			handanimator.ResetTrigger("hold");
+			handanimator.SetTrigger("unhold");
+		}
+
+		if (Input.GetKeyDown (KeyCode.A)) 
+		{
+			handanimator.SetTrigger("hold");
+		}
+
+		if (Input.GetKeyDown (KeyCode.S)) 
+		{
+			handanimator.ResetTrigger("hold");
+			handanimator.SetTrigger("unhold");
+			handanimator.ResetTrigger("unhold");
+		}
+
+	}
+}
