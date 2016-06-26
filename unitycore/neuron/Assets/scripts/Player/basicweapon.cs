d29c34306b6ecb7d09ae4a92fa90aec278a67e52
sirerr <sirerr@gmail.com>
2016-06-26 01:57:04 -0400

    basic attack is working, importing playmaker too

--- unitycore/neuron/Assets/scripts/Player/basicweapon.cs
+++ unitycore/neuron/Assets/scripts/Player/basicweapon.cs
@@ -1,15 +1,43 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class basicweapon : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
+	public List<GameObject> poppers = new List<GameObject>();
+	public int poppercount =0;
+
+	//emitter end
+	public GameObject emitter;
+	public GameObject popperobj;
+
+	//steam code
+	SteamVR_TrackedObject trackedobj;
+	SteamVR_Controller.Device deviceobj;
+
+	//attack paramaters
+	public float attackspeed =3f;
+
+	void Awake()
+	{
+		poppercount = poppers.Count;
+		for (int i = 0; i < poppercount; i++) {
+			poppers [i] = popperobj;
+		}
+
+		trackedobj = transform.parent.GetComponent<SteamVR_TrackedObject>();
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+	void FixedUpdate()
+	{
+		deviceobj = SteamVR_Controller.Input((int)trackedobj.index);
+
+		if(deviceobj.GetPressDown(SteamVR_Controller.ButtonMask.Trigger))
+		{
+			GameObject attack;
+			attack = Instantiate (popperobj, emitter.transform.position, Quaternion.identity) as GameObject;
+			Rigidbody rbody = attack.GetComponent<Rigidbody> ();
+			rbody.velocity = emitter.transform.forward * attackspeed;
+		}
 	}
 }
